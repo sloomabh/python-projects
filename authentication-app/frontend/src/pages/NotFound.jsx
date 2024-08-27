a1d58a0004244cf3c7182a12685f9e04453ee1ba
pej.benhalima <pej.benhalima@gmail.com>
2024-08-27 14:55:42 +0900

    connect to backend

---
+++ authentication-app/frontend/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>The page you`&apos;`re looking for doesn`&apos;`t exist!</p>
+    </div>
+  );
+}
+
+export default NotFound;
